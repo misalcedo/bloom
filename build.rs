d6e74d187488a086ce6827af49aa1403c2b0bfed
Miguel D. Salcedo <misalcedo@github.com>
2021-06-17 11:29:55 -0400

    Add initial build.rs

--- build.rs
+++ build.rs
@@ -2,11 +2,28 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
+
+/// Read configuration information from ruby executable.
+fn ruby_configuration(key: &str) -> String {
+    let config = Command::new("ruby")
+        .arg("-e")
+        .arg(format!("print RbConfig::CONFIG['{}']", key))
+        .output()
+        .unwrap_or_else(|e| panic!("ruby not found: {}", e));
+
+    String::from_utf8(config.stdout).expect("Configuration value is not valid UTF-8.")
+}
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-    println!("cargo:rustc-link-lib=ruby");
+    let ruby_h_path = ruby_configuration("rubyhdrdir");
+    let platform_h_path = ruby_configuration("rubyarchhdrdir");
+    let ruby_lib_path = ruby_configuration("libdir");
+    let ruby_library = ruby_configuration("RUBY_SO_NAME");
+
+    // Tell cargo to tell rustc to link the ruby library.
+    println!("cargo:rustc-link-search={}", ruby_lib_path);
+    println!("cargo:rustc-link-lib=dylib={}", ruby_library);
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
@@ -18,6 +35,30 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .clang_arg(format!("-I{}", ruby_h_path))
+        .clang_arg(format!("-I{}", platform_h_path))
+        .clang_arg("-fdeclspec")
+        .clang_arg("-Wall")
+        .clang_arg("-Wextra")
+        .clang_arg("-Wdeprecated-declarations")
+        .clang_arg("-Wdivision-by-zero")
+        .clang_arg("-Wimplicit-function-declaration")
+        .clang_arg("-Wimplicit-int")
+        .clang_arg("-Wpointer-arith")
+        .clang_arg("-Wshorten-64-to-32")
+        .clang_arg("-Wwrite-strings")
+        .clang_arg("-Wmissing-noreturn")
+        .clang_arg("-Wno-constant-logical-operand")
+        .clang_arg("-Wno-long-long")
+        .clang_arg("-Wno-missing-field-initializers")
+        .clang_arg("-Wno-overlength-strings")
+        .clang_arg("-Wno-parentheses-equality")
+        .clang_arg("-Wno-self-assign")
+        .clang_arg("-Wno-tautological-compare")
+        .clang_arg("-Wno-unused-parameter")
+        .clang_arg("-Wno-unused-value")
+        .clang_arg("-Wunused-variable")
+        .clang_arg("-Wextra-tokens")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
