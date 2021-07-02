d22fdd4623cffe71aa9a44e9b0b12a8a6463cbec
Miguel D. Salcedo <misalcedo@github.com>
2021-07-02 15:43:52 -0400

    Implement contains multiple on the Rust bloom filters.

--- src/bloom_filter.rs
+++ src/bloom_filter.rs
@@ -1,5 +1,5 @@
 use crate::errors::{BloomFilterError, ErrorKind};
-use crate::hash::{indices, sha512};
+use crate::hash::{indices, sha512, sha512_multiple};
 use bitvec::prelude::*;
 
 pub struct BloomFilter {
@@ -43,11 +43,15 @@ impl BloomFilter {
     /// The value may be any borrowed form of the filter's value type, but
     /// [`Eq`] on the borrowed form *must* match those for the value type.
     pub fn contains(&self, value: &[u8]) -> bool {
-        let mut contained = true;
-        let n = self.counts.len();
         let output = sha512(value);
-        let indices = indices(&output);
+        self.is_marked(output)
+    }
 
+    fn is_marked(&self, hash: [u8; 64]) -> bool {
+        let indices = indices(&hash);
+
+        let n = self.counts.len();
+        let mut contained = true;
         for i in indices {
             if let Some(marked) = self.counts.get(i % n) {
                 contained = *marked && contained;
@@ -67,6 +71,19 @@ impl BloomFilter {
     /// Not all bloom filters support removal of elements.
     pub fn remove(&self, _value: &[u8]) -> Result<bool, BloomFilterError> {
         Err(ErrorKind::NotSupported.into())
+    }
+
+    /// Returns `true` if the filter contains all the values.
+    ///
+    /// The values may be any borrowed form of the filter's value type, but
+    /// [`Eq`] on the borrowed form *must* match those for the value type.
+    pub fn contains_multiple(&self, values: &[&[u8]]) -> Result<Vec<bool>, BloomFilterError> {
+        let hashes = sha512_multiple(values)?;
+
+        Ok(hashes
+            .into_iter()
+            .map(|hash| self.is_marked(hash))
+            .collect())
     }
 }
 
@@ -89,6 +106,33 @@ mod tests {
         filter.insert("1".as_bytes());
 
         assert!(filter.contains("1".as_bytes()));
+    }
+
+    #[test]
+    fn when_empty_contains_multiple() {
+        let filter: BloomFilter = BloomFilter::new(1);
+        let value = "1";
+        let values = vec![value.as_bytes(); 100];
+
+        let result = filter.contains_multiple(&values);
+
+        assert!(result.is_ok());
+        assert!(result.unwrap().iter().all(|&is_member| !is_member));
+    }
+
+    #[test]
+    fn when_is_colliding_member_contains_multiple() {
+        let mut filter = BloomFilter::new(1);
+        let value = "1";
+
+        filter.insert("1".as_bytes());
+
+        let values = vec![value.as_bytes(); 100];
+
+        let result = filter.contains_multiple(&values);
+
+        assert!(result.is_ok());
+        assert!(result.unwrap().iter().all(|&is_member| is_member));
     }
 
     #[test]
