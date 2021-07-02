09a6c8eec9d5ea0470f2bea1943cea76da43df8f
Miguel D. Salcedo <misalcedo@github.com>
2021-07-02 19:50:42 -0400

    Reuse indices_of code.

--- lib/bloom_ruby.rb
+++ lib/bloom_ruby.rb
@@ -3,9 +3,13 @@ require "openssl"
 module BloomRuby
   BloomFilterError = Class.new(StandardError)
 
+  def self.indices_of(value, digest="SHA512")
+    digest = OpenSSL::Digest.new(digest).digest(value.to_s)
+    digest.unpack("J*")
+  end
+
   class BloomFilter
     def initialize(capacity)
-      @digest = "SHA512"
       @markers = Array.new(capacity, false)
     end
 
@@ -16,7 +20,7 @@ module BloomRuby
     def add(value)
       contained = true
 
-      indices_of(value).each do |i|
+      BloomRuby::indices_of(value).each do |i|
         index = i % self.capacity
         contained &&= @markers[index]
         @markers[index] = true
@@ -26,7 +30,7 @@ module BloomRuby
     end
 
     def include?(value)
-      indices_of(value).all? { |i| @markers[index = i % self.capacity] }
+      BloomRuby::indices_of(value).all? { |i| @markers[index = i % self.capacity] }
     end
 
 
@@ -37,18 +41,10 @@ module BloomRuby
     def delete(value)
       raise(BloomFilterError, "Bloom filter does not support the #delete operation.")
     end
-
-    private
-
-    def indices_of(value)
-      digest = OpenSSL::Digest.new(@digest).digest(value.to_s)
-      digest.unpack("J*")
-    end
   end
 
   class AtomicBloomFilter
     def initialize(capacity)
-      @digest = "SHA512"
       @semaphore = Mutex.new
       @markers = Array.new(capacity, false)
     end
@@ -61,7 +57,7 @@ module BloomRuby
       contained = true
 
       @semaphore.synchronize do
-        indices_of(value).each do |i|
+        BloomRuby::indices_of(value).each do |i|
           index = i % self.capacity
           contained &&= @markers[index]
           @markers[index] = true
@@ -73,7 +69,7 @@ module BloomRuby
 
     def include?(value)
       @semaphore.synchronize do
-        indices_of(value).all? { |i| @markers[index = i % self.capacity] }
+        BloomRuby::indices_of(value).all? { |i| @markers[index = i % self.capacity] }
       end
     end
 
@@ -83,13 +79,6 @@ module BloomRuby
 
     def delete(value)
       raise(BloomFilterError, "Bloom filter does not support the #delete operation.")
-    end
-
-    private
-
-    def indices_of(value)
-      digest = OpenSSL::Digest.new(@digest).digest(value.to_s)
-      digest.unpack("J*")
     end
   end
 end
